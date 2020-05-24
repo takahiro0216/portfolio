6de585ace2a5659ed7387cef3c1711b81aeddc8f
owner <mimura.takahiro.0216@gmail.com>
2020-05-24 16:54:57 +0900

    tuika

---
+++ portfolio2/index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+if(!empty($_POST)){
+  $_SESSION['url'] = $_POST['url'];
+  header('Location: test.php');
+  exit();
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>検索</title>
+</head>
+<body>
+  <form action="" method="post">
+    <input type="text"  name="url" placeholder="URLを入力">
+    <input type="submit" value="GO!" >  
+  
+  </form>
+</body>
+</html>
---
+++ portfolio2/test.php
@@ -0,0 +1,42 @@
+<?php
+require_once("phpQuery-onefile.php");
+session_start();
+
+$file = fopen("data.csv" ,"a");
+
+$html = file_get_contents("$_SESSION[url]");
+
+$title_num = count(phpQuery::newDocument($html)->find(".product-title"));
+$price_num = count(phpQuery::newDocument($html)->find(".price"));
+
+
+for($i=0,$p=1; $p<$price_num; $i++,$p++){
+  $titles = phpQuery::newDocument($html)->find(".product-title:eq($i)")->text();
+  $price = phpQuery::newDocument($html)->find(".price:eq($p)")->text();
+  
+
+    fwrite($file,$titles.$price."\n");
+  
+
+  sleep(3);
+}
+
+
+
+fclose($file);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1>リサーチ完了！</h1>
+  <p>csvはMAMPのhtdocs→iHerbの中にあるよ！</p>
+</body>
+</html>
